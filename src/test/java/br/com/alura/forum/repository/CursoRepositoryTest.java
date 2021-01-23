fa6c1540b06ba2f41775d8c5f536e00dbd049522
lucas_galo <lucas_d>
2021-01-23 09:13:54 -0300

    populando o banco para o cenário de test

--- src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -19,10 +20,20 @@ public class CursoRepositoryTest {
     @Autowired
     private CursoRepository repository;
 
+    @Autowired
+    private TestEntityManager em;
+
     @Test
     public void deveriaCarregarUmCursoAoBuscarPeloSeuNome() {
 
         String nomeCurso = "HTML 5";
+
+        // populando o banco de acordo com o cenário
+        Curso html5 = new Curso();
+        html5.setNome(nomeCurso);
+        html5.setCategoria("Programacao");
+        em.persist(html5);
+
         Curso curso = repository.findByNome(nomeCurso);
         Assert.assertNotNull(curso);
         Assert.assertEquals(nomeCurso, curso.getNome());
